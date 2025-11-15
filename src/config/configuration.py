e54a4284e41536954ece5873246b47246c7c9ab8
Dinesh Kumar Verma <vermadinesh006@gmail.com>
2025-11-15 18:05:57 +0530

    streamlit app

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -3,7 +3,12 @@ import sys
 from src.logger.log import logging
 from src.utils.util import read_yaml_file
 from src.exception.exception_handler import AppException
-from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from src.entity.config_entity import (DataIngestionConfig,
+                                      DataValidationConfig,
+                                      DataTransformationConfig,
+                                      ModelTrainerConfig,
+                                      ModelRecommendationConfig
+                                      )
 from src.constant import *
 
 
@@ -112,6 +117,36 @@ class AppConfiguration:
             )
 
             logging.info(f"Model Trainer Config: {response}")
+            return response
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+
+    def get_recommendation_config(self) -> ModelRecommendationConfig:
+        try:
+            model_trainer_config = self.configs_info['model_trainer_config']
+            data_validation_config = self.configs_info['data_validation_config']
+            trained_model_name = model_trainer_config['trained_model_name']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+            trained_model_dir = os.path.join(artifacts_dir, model_trainer_config['trained_model_dir'])
+        
+            
+
+            book_name_serialized_objects = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'], 'book_names.pkl')
+            book_pivot_serialized_objects = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'], 'book_pivot.pkl')
+            final_rating_serialized_objects = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'], 'final_rating.pkl')
+
+            trained_model_path = os.path.join(trained_model_dir,trained_model_name)
+          
+            response = ModelRecommendationConfig(
+                book_name_serialized_objects = book_name_serialized_objects,
+                book_pivot_serialized_objects = book_pivot_serialized_objects,
+                final_rating_serialized_objects = final_rating_serialized_objects,
+                trained_model_path = trained_model_path
+            )
+
+            logging.info(f"Model Recommendation Config: {response}")
             return response
 
         except Exception as e:
